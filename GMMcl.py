3a0d1b1445392195a0f49a3a8ea79ce592d094e2
Joachim Thiemann <jthiem@users.noreply.github.com>
2019-04-24 23:01:51 +0200

    Added CC-BY 4.0 text.

--- GMMcl.py
+++ GMMcl.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
-"""GMM based clssifier"""
+"""GMM based classifier
+
+(c) 2019 Joachim Thiemann
+
+This work is licensed under the Creative Commons Attribution-ShareAlike 
+4.0 International License.  To view a copy of this license, visit 
+http://creativecommons.org/licenses/by-sa/4.0/ or send a letter to 
+Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+"""
 import numpy as np
 import sklearn.base
 from sklearn.mixture import GMM
